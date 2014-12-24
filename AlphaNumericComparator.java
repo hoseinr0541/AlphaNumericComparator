3ebe664c64c8e9dabf917aa4633fc1824e232149
Kannan Ramamoorthy <kannangce@rediffmail.com>
2014-12-24 07:26:49 +0530

    + Added contents to README.md and fixed the comments.

--- AlphaNumericComparator.java
+++ AlphaNumericComparator.java
@@ -7,19 +7,26 @@ import java.util.List;
 
 /**
  * Sorter that compares the given Alpha-numeric strings. This iterates through
- * each characters to decide the sort order. There are 3 possible cases while
+ * each characters to decide the sort order. There are 2 possible cases while
  * iterating,
  * 
- * <li>
+ * <li>When the current character under comparison in at-least one of the given
+ * String is a non-digit the particular character alone compared lexically.</li>
  * 
- * <li>At any point if the comparison gives the order(either > or <) then the
- * consecutive characters will not be considered.</li>
+ * <li>When the current character under comparison in both of the String is a
+ * digit, the consecutive digit characters in both the Strings will be
+ * considered for comparison.</li>
+ * 
+ * <li>At any point if the comparison confirms the order(either > or <) then
+ * the consecutive characters will not be considered.</li>
  * 
  * For ex., this will be the ordered O/P of the given list of Strings.(The bold
  * characters decides its order)
- * <i><b>2</b>b,<b>100</b>b,a<b>1</b>,A<b>2</b>y,a<b>100</b>,</i>
+ * <i>
+ * 	<b>2</b>b,<b>100</b>b,a<b>1</b>,A<b>2</b>y,a<b>100</b>
+ * </i>
  * 
- * @author kannan_r
+ * @author kannangce
  * 
  */
 public class AlphaNumericComparator implements Comparator<String>
